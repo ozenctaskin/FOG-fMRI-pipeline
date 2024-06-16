222449ddffddc08694a43f488dfb6c715a50ebf6
Ozzy <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2024-06-15 20:05:59 -0400

    Add blurring to pipeline

--- FOGpipeline.m
+++ FOGpipeline.m
@@ -14,10 +14,10 @@ runReconAll('/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG
 runReconAll('/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG8/ses-CLR/anat/sub-pFOG8_ses-CLR_acq-MP2RAGE_UNIT1_unbiased_clean.nii', 'sub-pFOG8');
 
 
-%% Process subjects
-% Run func
+%% Process subjects without blur
+% Specify folders and subjects
 dataFolder = '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/';
-templatePath = fullfile(getenv('FSLDIR'), 'data', 'standard', 'MNI152_T1_1mm.nii.gz');
+templatePath = fullfile(getenv('FSLDIR'), 'data', 'standard', 'MNI152_T1_1mm_brain.nii.gz');
 subjects = {'sub-pFOG3', 'sub-pFOG4', 'sub-pFOG6', 'sub-pFOG7', 'sub-pFOG8'};
 anatomicals = {'/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG3/ses-PPN/anat/sub-pFOG3_ses-PPN_acq-MP2RAGE_UNIT1_unbiased_clean.nii', ...
                '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG4/ses-PPN/anat/sub-pFOG4_ses-PPN_acq-MPRAGE_rec-RMS_run-2_part-mag_T1w.nii.gz', ...
@@ -25,15 +25,43 @@ anatomicals = {'/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-p
                '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG7/ses-PPN/anat/sub-pFOG7_ses-PPN_acq-MP2RAGE_UNIT1_unbiased_clean.nii', ...
                '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG8/ses-CLR/anat/sub-pFOG8_ses-CLR_acq-MP2RAGE_UNIT1_unbiased_clean.nii'};
 sessions = {{'ses-PPN'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}};
+blurFWHM = 'NA';
 
+% Loop through subjects and sessions and run the analysis
 for sub = 1:length(subjects)
     subjectID = subjects{sub};
     anatomicalPath = anatomicals{sub};
     for ses = 1:length(sessions{sub})
         sessionID = sessions{sub}{ses};
         runNumber = '1';
-        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath)
+        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
         runNumber = '2';
-        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath)
+        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
+    end
+end
+
+%% Process subjects with blur
+% Specify folders and subjects
+dataFolder = '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/';
+templatePath = fullfile(getenv('FSLDIR'), 'data', 'standard', 'MNI152_T1_1mm_brain.nii.gz');
+subjects = {'sub-pFOG3', 'sub-pFOG4', 'sub-pFOG6', 'sub-pFOG7', 'sub-pFOG8'};
+anatomicals = {'/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG3/ses-PPN/anat/sub-pFOG3_ses-PPN_acq-MP2RAGE_UNIT1_unbiased_clean.nii', ...
+               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG4/ses-PPN/anat/sub-pFOG4_ses-PPN_acq-MPRAGE_rec-RMS_run-2_part-mag_T1w.nii.gz', ...
+               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG6/ses-CLR/anat/sub-pFOG6_ses-CLR_acq-MPRAGE_rec-RMS_run-2_part-mag_T1w.nii.gz', ...
+               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG7/ses-PPN/anat/sub-pFOG7_ses-PPN_acq-MP2RAGE_UNIT1_unbiased_clean.nii', ...
+               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG8/ses-CLR/anat/sub-pFOG8_ses-CLR_acq-MP2RAGE_UNIT1_unbiased_clean.nii'};
+sessions = {{'ses-PPN'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}};
+blurFWHM = '4';
+
+% Loop through subjects and sessions and run the analysis
+for sub = 1:length(subjects)
+    subjectID = subjects{sub};
+    anatomicalPath = anatomicals{sub};
+    for ses = 1:length(sessions{sub})
+        sessionID = sessions{sub}{ses};
+        runNumber = '1';
+        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
+        runNumber = '2';
+        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
     end
 end
