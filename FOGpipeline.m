0003fd2368f6fb328798cb5960998a0c0d7a0a98
Ozzy <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2024-07-03 12:29:56 -0400

    Processing of healthy and patients

--- FOGpipeline.m
+++ FOGpipeline.m
@@ -13,8 +13,7 @@ runReconAll('/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG
 runReconAll('/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG7/ses-PPN/anat/sub-pFOG7_ses-PPN_acq-MP2RAGE_UNIT1_unbiased_clean.nii', 'sub-pFOG7');
 runReconAll('/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG8/ses-CLR/anat/sub-pFOG8_ses-CLR_acq-MP2RAGE_UNIT1_unbiased_clean.nii', 'sub-pFOG8');
 
-
-%% Process subjects without blur
+%% Process healthy subjects
 % Specify folders and subjects
 dataFolder = '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/';
 templatePath = fullfile(getenv('FSLDIR'), 'data', 'standard', 'MNI152_T1_1mm_brain.nii.gz');
@@ -34,26 +33,14 @@ for sub = 1:length(subjects)
     for ses = 1:length(sessions{sub})
         sessionID = sessions{sub}{ses};
         runNumber = '1';
-        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
+        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, true)
         runNumber = '2';
         preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
     end
 end
 
-%% Process subjects with blur
-% Specify folders and subjects
-dataFolder = '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/';
-templatePath = fullfile(getenv('FSLDIR'), 'data', 'standard', 'MNI152_T1_1mm_brain.nii.gz');
-subjects = {'sub-pFOG3', 'sub-pFOG4', 'sub-pFOG6', 'sub-pFOG7', 'sub-pFOG8'};
-anatomicals = {'/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG3/ses-PPN/anat/sub-pFOG3_ses-PPN_acq-MP2RAGE_UNIT1_unbiased_clean.nii', ...
-               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG4/ses-PPN/anat/sub-pFOG4_ses-PPN_acq-MPRAGE_rec-RMS_run-2_part-mag_T1w.nii.gz', ...
-               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG6/ses-CLR/anat/sub-pFOG6_ses-CLR_acq-MPRAGE_rec-RMS_run-2_part-mag_T1w.nii.gz', ...
-               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG7/ses-PPN/anat/sub-pFOG7_ses-PPN_acq-MP2RAGE_UNIT1_unbiased_clean.nii', ...
-               '/home/chenlab-linux/Desktop/FOG/healthyControls/bidsFolder/sub-pFOG8/ses-CLR/anat/sub-pFOG8_ses-CLR_acq-MP2RAGE_UNIT1_unbiased_clean.nii'};
-sessions = {{'ses-PPN'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}, {'ses-PPN','ses-CLR'}};
+% Run the same thing with blur 4mm
 blurFWHM = '4';
-
-% Loop through subjects and sessions and run the analysis
 for sub = 1:length(subjects)
     subjectID = subjects{sub};
     anatomicalPath = anatomicals{sub};
@@ -75,7 +62,7 @@ dataFolder = '/home/chenlab-linux/Desktop/FOG/patients/bidsFolder/';
 templatePath = fullfile(getenv('FSLDIR'), 'data', 'standard', 'MNI152_T1_1mm_brain.nii.gz');
 subjects = {'sub-pFOGa'};
 anatomicals = {'/home/chenlab-linux/Desktop/FOG/patients/bidsFolder/sub-pFOGa/ses-PPNexcitatory/anat/sub-pFOGa_ses-PPNexcitatory_acq-MP2RAGE_UNIT1_unbiased_clean.nii'};
-sessions = {{'ses-PPNexcitatory','ses-PPNinhibitory'}};
+sessions = {{'ses-PPNexcitatory','ses-PPNinhibitory', 'ses-UFexcitatory', 'ses-UFinhibitory'}};
 blurFWHM = 'NA';
 % Loop through subjects and sessions and run the analysis
 for sub = 1:length(subjects)
@@ -84,7 +71,7 @@ for sub = 1:length(subjects)
     for ses = 1:length(sessions{sub})
         sessionID = sessions{sub}{ses};
         runNumber = '1';
-        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
+        preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, true)
         runNumber = '2';
         preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, templatePath, blurFWHM, false)
     end
