73f8b385f048830da2ecac873e2de95d506113ce
Ozzy <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2024-09-11 16:20:05 -0400

    Adding an option to skip PCA

--- preprocessMEsingleRun.m
+++ preprocessMEsingleRun.m
@@ -1,21 +1,23 @@
-function preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, MNITemplate, blur, addSliceTime)
+function preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anatomicalPath, MNITemplate, blur, addSliceTime, skipPCA)
 
     % IMPORTANT!!!! This script won't run if you do not start MATLAB from 
-    % your terminal. If you are on linux, run "matlab". If you are on mac,
-    % run "open /Applications/MATLAB_R2023b.app" (Change the R2023b with 
-    % your version). The reason is that when started with the icon instead
-    % of the terminal, matlab cannot access your $PATH variables and uses 
-    % its own paths, so afni functions can't be found.
+    % your terminal. If you are on linux, run "matlab" on your terminal. 
+    % If you are on mac, run "open /Applications/MATLAB_R2023b.app" 
+    % (Change the R2023b with your version). The reason is that when 
+    % started with the icon instead of the terminal, matlab cannot access 
+    % your $PATH variables and uses its own paths, so afni functions can't 
+    % be found.
     %
-    % Note: For resting state preprocessing only. You need to run the 
-    % runReconAll.m function first for this to work.
+    % Note: This function is for resting state preprocessing only. 
+    % You need to run the runReconAll.m function first for this to work.
     %
     % This script performs preprocessing on multi-echo images with AFNI. 
     % Use it if you want to analyze every run separately. This is
     % applicable for instance if you take the subject out of the scanner in
     % the middle of the scan and therefore have multiple fieldmap images. 
     % In the results folder look for final_func.nii and final_anat.nii
-    % files. These are your preprocessed final output converted to NIFTI. 
+    % files. These are your preprocessed final output converted to NIFTI 
+    % format. 
     %
     %   dataFolder: BIDS folder where your subjects are located
     %   subjectID: Name of the subject folder located in dataFolder. e.g
@@ -26,6 +28,10 @@ function preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anat
     %   anatomicalPath: Path to anatomical image you want to process
     %   MNITemplate: You can find one in $FSLDIR/data/standard. use 1mm MNI
     %   use the _brain one. 
+    %   skipPCA: Our multi-echo processing removes some noise. The
+    %   literature is not solid whether additional PCA analysis is
+    %   beneficial after this, so we leave this optional. AFNI examples do
+    %   this operation though
     
     % Set up variables we will use 
     ventricles = fullfile(getenv('SUBJECTS_DIR'), subjectID, 'SUMA', 'fs_ap_latvent.nii.gz');
@@ -53,88 +59,50 @@ function preprocessMEsingleRun(dataFolder, subjectID, sessionID, runNumber, anat
     % info to the main body
     %    '-blur_size 4 ' ..., 
     %    '-blur_in_mask yes ' ..., 
-    if strcmp(blur, 'NA')
-        afni_line = ['cd ' fullfile(dataFolder, subjectID, sessionID) ';' 'afni_proc.py ' ...,
-        '-subj_id ' subjectID ' ' ...,
-        '-blocks despike tshift align tlrc volreg mask combine scale regress ' ...,
-        '-radial_correlate_blocks tcat volreg ' ...,
-        '-copy_anat ' anatomicalPath ' '  ...,
-        '-anat_has_skull yes ' ..., 
-        '-anat_follower_ROI FSvent epi ' ventricles ' ' ...,      
-        '-anat_follower_ROI FSWe epi ' white_matter ' ' ...,            
-        '-anat_follower_erode FSvent FSWe ' ...,
-        '-blip_forward_dset ' blipForward ' ' ..., 
-        '-blip_reverse_dset ' blipReverse ' ' ..., 
-        '-dsets_me_run ' funcDataset ' ' ..., 
-        '-tshift_interp -wsinc9 ' ..., 
-        '-align_unifize_epi local ' ..., 
-        '-align_opts_aea -cost lpc+ZZ -giant_move -check_flip ' ...,
-        '-tlrc_base ' MNITemplate ' ' ..., 
-        '-tlrc_NL_warp ' ..., 
-        '-tlrc_no_ss ' ...,
-        '-volreg_align_to MIN_OUTLIER ' ...,
-        '-volreg_align_e2a ' ..., 
-        '-volreg_tlrc_warp ' ...,
-        '-volreg_post_vr_allin yes ' ..., 
-        '-volreg_pvra_base_index MIN_OUTLIER ' ...,
-        '-volreg_compute_tsnr yes ' ..., 
-        '-volreg_warp_dxyz 2.5 ' ...,
-        '-combine_method m_tedana_m_tedort -echo_times 13 30 46 -reg_echo 2 ' ..., 
-        '-mask_epi_anat yes ' ...,
-        '-regress_motion_per_run ' ...,
-        '-regress_ROI_PC FSvent 3 ' ..., 
-        '-regress_ROI_PC_per_run FSvent ' ..., 
-        '-regress_anaticor_fast ' ...,
-        '-regress_anaticor_label FSWe ' ...,
-        '-regress_censor_motion 0.2 ' ...,
-        '-regress_censor_outliers 0.05 ' ..., 
-        '-regress_apply_mot_types demean deriv ' ..., 
-        '-regress_est_blur_epits ' ...,
-        '-regress_est_blur_errts ' ..., 
-        '-html_review_style pythonic ' ..., 
-        '-remove_preproc_files'];
-    else
-        afni_line = ['cd ' fullfile(dataFolder, subjectID, sessionID) ';' 'afni_proc.py ' ...,
-        '-subj_id ' subjectID ' ' ...,
-        '-blocks despike tshift align tlrc volreg mask combine blur scale regress ' ...,
-        '-radial_correlate_blocks tcat volreg ' ...,
-        '-copy_anat ' anatomicalPath ' '  ...,
-        '-anat_has_skull yes ' ..., 
-        '-anat_follower_ROI FSvent epi ' ventricles ' ' ...,      
-        '-anat_follower_ROI FSWe epi ' white_matter ' ' ...,            
-        '-anat_follower_erode FSvent FSWe ' ...,
-        '-blip_forward_dset ' blipForward ' ' ..., 
-        '-blip_reverse_dset ' blipReverse ' ' ..., 
-        '-dsets_me_run ' funcDataset ' ' ..., 
-        '-tshift_interp -wsinc9 ' ..., 
-        '-align_unifize_epi local ' ..., 
-        '-align_opts_aea -cost lpc+ZZ -giant_move -check_flip ' ...,
-        '-tlrc_base ' MNITemplate ' ' ..., 
-        '-tlrc_NL_warp ' ..., 
-        '-tlrc_no_ss ' ...,
-        '-volreg_align_to MIN_OUTLIER ' ...,
-        '-volreg_align_e2a ' ..., 
-        '-volreg_tlrc_warp ' ...,
-        '-volreg_post_vr_allin yes ' ..., 
-        '-volreg_pvra_base_index MIN_OUTLIER ' ...,
-        '-volreg_compute_tsnr yes ' ..., 
-        '-volreg_warp_dxyz 2.5 ' ...,
-        '-combine_method m_tedana_m_tedort -echo_times 13 30 46 -reg_echo 2 ' ..., 
-        '-mask_epi_anat yes ' ...,
-        '-regress_motion_per_run ' ...,
-        '-regress_ROI_PC FSvent 3 ' ..., 
-        '-regress_ROI_PC_per_run FSvent ' ..., 
-        '-regress_anaticor_fast ' ...,
-        '-regress_anaticor_label FSWe ' ...,
-        '-regress_censor_motion 0.2 ' ...,
-        '-regress_censor_outliers 0.05 ' ..., 
-        '-regress_apply_mot_types demean deriv ' ..., 
-        '-regress_est_blur_epits ' ...,
-        '-regress_est_blur_errts ' ..., 
-        '-html_review_style pythonic ' ..., 
-        '-remove_preproc_files ' ...,
-        '-blur_size ' blur ' ' ..., 
-        '-blur_in_mask yes']; 
+    afni_line = ['cd ' fullfile(dataFolder, subjectID, sessionID) ';' 'afni_proc.py ' ...,
+    '-subj_id ' subjectID ' ' ...,
+    '-blocks despike tshift align tlrc volreg mask combine scale regress ' ...,
+    '-radial_correlate_blocks tcat volreg ' ...,
+    '-copy_anat ' anatomicalPath ' '  ...,
+    '-anat_has_skull yes ' ..., 
+    '-anat_follower_ROI FSvent epi ' ventricles ' ' ...,      
+    '-anat_follower_ROI FSWe epi ' white_matter ' ' ...,            
+    '-anat_follower_erode FSvent FSWe ' ...,
+    '-blip_forward_dset ' blipForward ' ' ..., 
+    '-blip_reverse_dset ' blipReverse ' ' ..., 
+    '-dsets_me_run ' funcDataset ' ' ..., 
+    '-tshift_interp -wsinc9 ' ..., 
+    '-align_unifize_epi local ' ..., 
+    '-align_opts_aea -cost lpc+ZZ -giant_move -check_flip ' ...,
+    '-tlrc_base ' MNITemplate ' ' ..., 
+    '-tlrc_NL_warp ' ..., 
+    '-tlrc_no_ss ' ...,
+    '-volreg_align_to MIN_OUTLIER ' ...,
+    '-volreg_align_e2a ' ..., 
+    '-volreg_tlrc_warp ' ...,
+    '-volreg_post_vr_allin yes ' ..., 
+    '-volreg_pvra_base_index MIN_OUTLIER ' ...,
+    '-volreg_compute_tsnr yes ' ..., 
+    '-volreg_warp_dxyz 2.5 ' ...,
+    '-combine_method m_tedana_m_tedort -echo_times 13 30 46 -reg_echo 2 ' ..., 
+    '-mask_epi_anat yes ' ...,
+    '-regress_motion_per_run ' ...,
+    '-regress_censor_motion 0.2 ' ...,
+    '-regress_censor_outliers 0.05 ' ..., 
+    '-regress_apply_mot_types demean deriv ' ..., 
+    '-regress_est_blur_epits ' ...,
+    '-regress_est_blur_errts ' ..., 
+    '-html_review_style pythonic ' ..., 
+    '-remove_preproc_files'];
+    
+    % If blur is specified, add it to the function. 
+    if ~strcmp(blur, 'NA')
+        afni_line = [afni_line ' -blur_size ' blur ' -blur_in_mask yes'];
+    end
+    
+    % Check if PCA is skipped or not 
+    if skipPCA == 1
+        afni_line = [afni_line ' -regress_ROI_PC FSvent 3 -regress_ROI_PC_per_run FSvent -regress_anaticor_fast -regress_anaticor_label FSWe'];
     end
 
     system(afni_line);
